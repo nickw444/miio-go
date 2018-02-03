7f70fc206e94e9930015ef6ecb356d65b78f4008
Nick Whyte <nick@nickwhyte.com>
2018-02-03 15:28:02 +1100

    .

--- ci/check_mocks.sh
+++ ci/check_mocks.sh
@@ -4,6 +4,8 @@
 
 set -euo pipefail
 
+go get github.com/vektra/mockery/.../
+export PATH=$PATH:$GOPATH/bin
 make mocks
 if ! git diff --exit-code HEAD; then
   error "Mocks are not up to date, please run: make mocks"
