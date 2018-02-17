e7105ddcec1b79a384b4f4c90b98c9c1a860ac7f
Nick Whyte <nick@nickwhyte.com>
2018-02-17 16:12:07 +1100

    Add wireshark wssdl dissector

---
+++ tools/wireshark/miio.lua
@@ -0,0 +1,24 @@
+-- A Wireshark dissector written in for https://github.com/diacritic/wssdl.
+-- Install by adding to ~/.config/wireshark/plugins along with wssdl.
+
+local wssdl = require 'wssdl'
+
+foo = wssdl.packet
+{
+  magic     : u16()
+            : hex();
+  length    : u16();
+  unknown   : u32()
+            : hex();
+  deviceId  : u32()
+            : hex();
+  stamp     : u32();
+  checksum  : bytes(16);
+  data      : payload(magic);
+}
+
+wssdl.dissect {
+  udp.port:set {
+    [54321] = miio:proto('miio', 'Xiaomi Mi Home Binary Protocol')
+  }
+}
