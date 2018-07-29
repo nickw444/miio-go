523f5abf00f5c28945fbbdf89b1def1cb496372d
Nick Whyte <nick@nickwhyte.com>
2018-07-29 17:57:59 +1000

    Fix build + update dependencies

--- bin/check_mocks.sh
+++ bin/check_mocks.sh
@@ -5,7 +5,7 @@ ROOT_DIR=$(cd "$(dirname "$0")"/.. && pwd)
 cd ${ROOT_DIR}
 
 ./bin/regenerate_mocks.sh
-if ! git diff --exit-code HEAD; then
+if ! git diff --exit-code HEAD -- '*.go'; then
   echo "Mocks are not up to date, please run: make mocks"
   exit 1
 fi
--- bin/install_tools.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-go get -u github.com/haya14busa/goverage
-go get -u github.com/vektra/mockery/.../
